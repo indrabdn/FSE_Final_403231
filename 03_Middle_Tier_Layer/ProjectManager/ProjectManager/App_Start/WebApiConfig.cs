40fcd65f0af570b6de2ac0481f9f95e9c9704429
Indraraj Majilya <indraraj.bdn@gmail.com>
2019-07-21 18:26:25 +0530

    Web APi project completed

---
+++ 03_Middle_Tier_Layer/ProjectManager/ProjectManager/App_Start/WebApiConfig.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Formatting;
+using System.Web.Http;
+using ProjectManager.ActionFilters;
+using System.Web.Http.Cors;
+
+namespace ProjectManager
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            config.MapHttpAttributeRoutes();
+            config.EnableCors(new EnableCorsAttribute("*", "*", "*"));
+
+            config.IncludeErrorDetailPolicy = IncludeErrorDetailPolicy.Never;
+
+            config.Filters.Add(new ProjectManagerLogFilter());
+            config.Filters.Add(new ProjectManagerExceptionFilter());
+
+            var jsonFormatter = config.Formatters.OfType<JsonMediaTypeFormatter>().First();
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+            jsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
